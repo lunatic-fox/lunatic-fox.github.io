4b1a08910c948dc2e673da63d17875926074b035
joseliojunior <joseliojunior@users.noreply.github.com>
2021-03-13 14:00:42 -0300

    Bug fixed

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
 // To get a element by id with less code.   
 function id(id){return document.getElementById(id);};
 
-
-// To replace some tags with a chunk of HTML code. 
+// To replace some tags with a chunk of HTML code...
+/* 
+    That function search for all div elements with "tag" name and
+    then replace what's inside that element with a chunk of code
+    that shows the programming language used inside the project
+    and a lil' icon colored with color pattern of programming
+    languages used in GitHub.
+*/
 function playTag(){
-    const xy = document.querySelectorAll('.tag');
+    const tagLocation = document.querySelectorAll('div [name = "tag"]');
 
-    const vector = ['Kotlin','JavaScript','JSON','CSS','HTML','Java'];
+    const tagName = ['Kotlin','JavaScript','JSON','CSS','HTML','Java'];
 
-    for(i=0;i<=xy.length;i++){
-        for(j=0;j<=vector.length;j++){
-
-        xy[i].innerHTML = xy[i].innerHTML.split(`#${vector[j]}`).join(`<div class="fx tag-box">
-        <div class="language-description">${vector[j]}</div>
-        <div class="lang-icon ${vector[j]}"></div></div>`);
+    for(i=0;i<tagLocation.length;i++){        
+        for(j=0;j<tagName.length;j++){
+            tagLocation[i].innerHTML = tagLocation[i].innerHTML.split(`#${tagName[j]}`).join(
+                `<div class="fx tag-box">
+                    <div class="language-description">
+                        ${tagName[j]}
+                    </div>
+                    <div class="lang-icon ${tagName[j]}"></div>
+                </div>`
+            );    
         };
     };
 };
-
 
 // A object to wrap the functions that makes the id "info"
 // works like a switcher.
@@ -34,14 +43,14 @@ const info = {
     }
 };
 
-
 // That part makes the page starts without displaying id "off", then
 // creates two event listeners, one to id "on" and another to id "off".
 id('off').style='display:none;';
 id('on').addEventListener('click',()=>{info.on();});
 id('off').addEventListener('click',()=>{info.off();});
 
-
-// That part makes that function to work, but have a error
-// that I couldn't figure out how to fix it yet.
+// That part makes that function to work. Error fixed!
 playTag();
+
+// let ops = document.querySelector('div [name = "tag"]');
+// console.log(ops.innerHTML)
