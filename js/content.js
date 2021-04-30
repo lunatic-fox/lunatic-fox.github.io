29fa2b85f3df23b205e78ea9d55370f95f6c27db
joseliojunior <joseliojunior@users.noreply.github.com>
2021-04-30 07:21:20 -0300

    update

--- js/content.js
+++ js/content.js
@@ -1,132 +1,156 @@
+const content = document.getElementById('content');
+
+function requestPost(id) {
+    document.getElementById(id).addEventListener('click', ()=> {
+        requestJSON(id);
+    });
+};
+
 function contentConstructor(author, date, title) {
-    id('content').innerHTML = `
+    content.innerHTML = `
         <div id="content-header">
             <div id="content-bd-box" class="fx">
-                <div id="content-back"><img src="img/001.png" height="100%"></div>
+                <div id="content-back">
+                    <img src="img/001.png" height="100%">
+                </div>
                 <div id="content-description">
                     Criado por: <span id="content-author">${author}</span><br>
                     Última modificação em: <span id="content-date">${date}</span>
                 </div>
-                </div>
-            <div id="content-title-card" class="fx" >
+            </div>
+            <div id="content-title-card" class="fx">
                 <div id="content-title">${title}</div>
             </div>
-            <div id="content-body" class="fx blk">*</div>  
         </div>
-    </div>`;
+        <div id="content-body" class="fx blk">*</div>`;
 };
 
-function notFound() {
-    const img = 'img/000.png';
-    const title = '404';
-    const msg = 'Desculpe o inconveniente, provalvelmente essa parte está em construção, o autor esqueceu de vincular ou não teve tempo suficiente de carregar o conteúdo.';
-
-    id('content').innerHTML = `
-    <div id="no-content" class="fx">
-        <img src="${img}">
-        <div class="fx blk not-found-box">
-            <div class="not-found-title">${title}
-            </div>
-            <div class="not-found-msg">${msg}
-            </div>
-        </div>
-    </div>`;
-};
-
-function loadFeatures(path) {
-    if (path != '') {
-        const articles = document.getElementsByTagName('article');
+function contentBackButton(removeScript) {
+    function goBack() {
         for(i = 0; i < articles.length; i++) {
-            articles[i].classList = 'gone';
+            articles[i].classList = 'fx';
         };
+        content.classList = 'gone';
+        content.innerHTML = '';
+    };
 
-        const content = id('content');
-        content.classList = 'fx blk';
+    const contentBack = document.getElementById('content-back');
+    const articles = document.getElementsByTagName('article');
 
-        const node = document.createElement('script');
-        node.id = 'temp-script';
-        node.src = path;
-        document.body.appendChild(node);
-
-        id('content-back').addEventListener('click', ()=> {
-            document.body.removeChild(id('temp-script'));
-            
-            for(i = 0; i < articles.length; i++) {
-                articles[i].classList = 'fx';
-            };
-
-            content.classList = 'gone';
-            content.innerHTML = '';
+    if (removeScript != null) {
+        contentBack.addEventListener('click', ()=> {
+            document.body.removeChild(document.getElementById('temp-script'));
+            goBack();
         });
     } else {
-        const articles = document.getElementsByTagName('article');
-        for(i = 0; i < articles.length; i++) {
-            articles[i].classList = 'gone';
-        };
-
-        const content = id('content');
-        content.classList = 'fx blk';
-
-        id('content-back').addEventListener('click', ()=> {
-            for(i = 0; i < articles.length; i++) {
-                articles[i].classList = 'fx';
-            };
-
-            content.classList = 'gone';
-            content.innerHTML = '';
+        contentBack.addEventListener('click', ()=> {
+            goBack();
         });
     };
 };
 
-function content(path) {
+function contentNotFound() {
+    const img = 'img/003.png';
+    const msg = `<p> Infelizmente a página não foi encontrada. </p>
+    <p> É bem possível que o motivo disso estar ocorrendo seja por alguma manutenção da página. Mas anime-se, logo, logo tudo estará no seu devido lugar. </p>`;
+
+    const articles = document.getElementsByTagName('article');
+
+    for(i = 0; i < articles.length; i++) {
+        articles[i].classList = 'gone';
+    };
+    content.classList = 'fx blk';
+    content.innerHTML = `
+        <div id="content-header">
+            <div id="content-bd-box" class="fx">
+                <div id="content-back">
+                    <img src="img/001.png" height="100%">
+                </div>
+                <div class="not-found-title">404</div>
+            </div>
+        </div>
+        <div id="no-content" class="fx">
+            <img src="${img}">
+            <div class="fx blk not-found-box">
+                <div class="not-found-msg">${msg}</div>
+            </div>
+        </div>`;
+    contentBackButton();
+};
+
+function loadFeatures(scriptPath, titleCardHexColor) {
+    const articles = document.getElementsByTagName('article');
+    const titleCardColor = document.getElementById('content-title-card');
+
+    function goToPost() {
+        for(i = 0; i < articles.length; i++) {
+            articles[i].classList = 'gone';
+        };
+        content.classList = 'fx blk';
+    };
+
+    if (scriptPath != '') {
+        goToPost();
+
+        const node = document.createElement('script');
+        node.id = 'temp-script';
+        node.src = scriptPath;
+        document.body.appendChild(node);
+
+        contentBackButton(true);
+    } else {
+        goToPost();
+        contentBackButton();
+    };
+
+    if (titleCardHexColor != '') {
+        titleCardColor.style = `background: linear-gradient(transparent, ${titleCardHexColor});`;
+    };
+};
+
+function contentProvider(path) {
+    const contentBody = document.getElementById('content-body');
+
     const requestContent = new XMLHttpRequest();
     requestContent.open('GET', path);
     requestContent.send();
-    requestContent.onreadystatechange = ()=> {
-        if (requestContent.readyState == 4
-            && requestContent.status == 200) {
-                document.getElementById('content-body').innerHTML = requestContent.responseText;
+    requestContent.onreadystatechange = function() {
 
+        if (this.readyState == 4 && this.status == 200) {
+                contentBody.innerHTML = this.responseText;
                 playText();
-
-        } else if (requestContent.readyState == 4
-            && requestContent.status == 404){
-                notFound();
+        } else if (this.readyState == 4
+            && this.status == 404){
+                contentNotFound();
         } else {''};
+        
     };
 };
 
 function requestJSON(from) {
-    let obj = '';
+    let obj;
 
     const requestHeader = new XMLHttpRequest();
     requestHeader.open('GET', `json/${from}.json`);
     requestHeader.send();
-    requestHeader.onreadystatechange = ()=> {
+    requestHeader.onreadystatechange = function() {
 
-        if (requestHeader.readyState == 4 
-            && requestHeader.status == 200) {
+        if (this.readyState == 4 && this.status == 200) {
 
-                obj = JSON.parse(requestHeader.response);
+            obj = JSON.parse(this.response);
 
             contentConstructor(obj.author, obj.date, obj.title);
-            content(obj.contentPath);
+            contentProvider(obj.path.content);
 
-            ///////////////////////
             setTimeout(() => {
-                loadFeatures(obj.scriptPath);
+                loadFeatures(obj.path.script, obj.titleCardColor);
             }, 200);
-            ///////////////////////
 
-        } else if (requestHeader.readyState == 4 
-            && requestHeader.status == 404) {
-            notFound();
+        } else if (this.status == 404) {
+            contentNotFound();
         } else {''};
     };
         
 };
 
-
-id('p0002').addEventListener('click', ()=> {
-    requestJSON('p0002');
-});
+requestPost('p0002');
