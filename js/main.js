ef6bd79ee1b8f5d954ac7ba33fc499de7f5c96b7
joseliojunior <joseliojunior@users.noreply.github.com>
2021-05-25 20:37:02 -0300

    Update page

--- js/main.js
+++ js/main.js
@@ -1,63 +1,68 @@
-import {data, layout, post} from './classes.js';
+/**
+ * @author Josélio de S. C. Júnior <joseliojrx25@gmail.com>
+ * @copyright Josélio de S. C. Júnior 2021
+ */
+`use strict`
+import {data, layout, post} from './objects.js';
 
-const def = {
-    starThemeBtn : document.getElementById('star-theme-btn'),
-    themeLinkElement : document.getElementById('theme'),
-    cardBoxesOrientation : document.getElementsByName('post-orientation'),
-    pageStatus : 1,
-    pageMax : await data.size()
+const ids = {
+    CONTENT: 'content',
+    loading: document.getElementById('loading-screen'),
+    arrowGo: document.getElementById('arrow-go'),
+    arrowBack: document.getElementById('arrow-back'),
+    starThemeBtn: document.getElementById('star-theme-btn'),
+    themeLinkElement: document.getElementById('theme'),
+    cardBoxes: document.getElementsByName('post-orientation'),
+    numberText: document.getElementById('page-number'),
 };
 
-class main {
+const pagesDef = {
+    index: 1,
+    length: await data.size()
+};
 
-    static setLayout() {
-        layout.windowContext(def.cardBoxesOrientation);
-        layout.themePicker(def.starThemeBtn, def.themeLinkElement);
-        layout.footer;
-    };
-
-    static pageSelector(element, direction) {
+const main = {
+    init() {
+        this.loadEnd();
+        window.addEventListener('resize', this.setLayout);
+        this.selector(ids.arrowGo, ids.arrowBack);
+    },
+    setLayout() {
+        layout.windowContext(ids.cardBoxes);
+        layout.themePicker(ids.starThemeBtn, ids.themeLinkElement);
+    },
+    pageSelector(element, direction) {
         element.addEventListener('click', async ()=> {
-            if (direction && def.pageStatus < def.pageMax) def.pageStatus++;
+            if (direction && pagesDef.index < pagesDef.length)
+            pagesDef.index++;
 
-            if (!direction && def.pageStatus > 1)  def.pageStatus--;
+            if (!direction && pagesDef.index > 1)
+            pagesDef.index--;
 
-            await this.getData(def.pageStatus);
-            layout.windowContext(def.cardBoxesOrientation);
+            await this.getData(pagesDef.index);
+            ids.numberText.innerHTML = pagesDef.index;
+            layout.windowContext(ids.cardBoxes);
         });
-    };
-
-    static selector(goAhead, goBack) {
+    },
+    selector(goAhead, goBack) {
         this.pageSelector(goAhead, true);
         this.pageSelector(goBack, false);
-    };
+    },
+    async getData(page) {
+        const url = `${data.URL}${page ??= 1}.json`;
+        const capturedData = await fetch(url);
 
-    static async getData(page) {
-        const url = `https://joseliojunior.github.io/data/post/p${page ??= 1}.json`;
-        const data = await fetch(url);
-    
-        if (data.status == 404)
-        return post.error('content');
-    
-        const response = await data.json();
-        return post.page('content', response);
-    };
-    
-    static async setData() {
+        const response = await capturedData.json();
+        return post.buildContent(ids.CONTENT, response);
+    },
+    async setData() {
         await this.getData();
-        this.setLayout();    
-    };    
-
-    static async loadEnd() {
+        this.setLayout(); 
+    },
+    async loadEnd() {
         await this.setData();
-        return document.getElementById('transition').remove();
-    };
-
-    static init = this.loadEnd();
-    static resize = window.addEventListener('resize', this.setLayout);
-    static select =  this.selector(document.getElementById('pagego'), document.getElementById('pageback'));
+        return ids.loading.remove();
+    }
 };
 
-main.init;
-main.resize;
-main.select;
+main.init();
--- js/objects.js
+++ js/objects.js
@@ -1,33 +1,31 @@
+/**
+ * @author Josélio de S. C. Júnior <joseliojrx25@gmail.com>
+ * @copyright Josélio de S. C. Júnior 2021
+ */
+`use strict`
 import {lang} from './langColors.js';
-export {data, layout, post}
+export {data, layout, post};
 
-class data {
-    static async size() {
+const CONSOLE_INFO = 'color: #0d9c00; background: #0d9c0021; border-radius: 2px; font-size: 15px; font-weight: bold;';
+
+const data = {
+    URL: 'https://joseliojunior.github.io/data/post/p',
+    async size() {
         let n = 0;
-        for (let i = 1; i < 50; i++) {
-            if ((await fetch(`https://joseliojunior.github.io/data/post/p${i}.json`)).ok) n++;
-            else break;
+        for (let i = 1; i < 100; i++) {
+            if ((await fetch(`${this.URL}${i}.json`)).ok) n++;
+            else {
+                console.clear();
+                console.info(`%c Total pages counted: ${n} `, CONSOLE_INFO);
+                break;
+            };
         };
         return n;
-    };
+    }
 };
 
-class layout {
-
-    static footer = this.footerPosition();
-    
-    static footerPosition() {
-
-        const main = document.getElementsByTagName('main').item(0);
-        const footer = document.getElementsByTagName('footer').item(0);
-    
-        if (main.scrollHeight < innerHeight - 50)
-        return footer.style.position = 'fixed';
-        else return footer.style.position = 'unset';
-
-    };
-
-    static themePicker(input, output) {
+const layout = {
+    themePicker(input, output) {
         const KEY = 'io.github.joseliojunior.theme';
         const DARKMODE = 'css/theme-dark.css';
         const LIGHTMODE = 'css/theme-light.css';
@@ -48,9 +46,9 @@ class layout {
                 status = true;
             };
         });  
-    };
+    },
 
-    static windowContext(postCards) {
+    windowContext(postCards) {
         const THRESHOLD = 768;
     
         if (innerWidth > THRESHOLD) {
@@ -62,24 +60,46 @@ class layout {
                 postCards[i].classList = 'flexblock';
             };
         };
-    };
+    }
 };
 
+const post = {
+    blankCard: `<div style="width: 90%; margin: 10px;"><div></div>`,
+    buildContent(context, p) {
+        document.getElementById(context).innerHTML = `
+        ${this.box(
+            this.card(p?.c1.title, p?.c1.description, p?.c1.href, p?.c1.tags),
+            this.card(p?.c2.title, p?.c2.description, p?.c2.href, p?.c2.tags)
+        )}
+        ${this.box(
+            this.card(p?.c3.title, p?.c3.description, p?.c3.href, p?.c3.tags),
+            this.card(p?.c4.title, p?.c4.description, p?.c4.href, p?.c4.tags)
+        )}
+        ${this.box(
+            this.card(p?.c5.title, p?.c5.description, p?.c5.href, p?.c5.tags),
+            this.card(p?.c6.title, p?.c6.description, p?.c6.href, p?.c6.tags)
+        )}
+        ${this.box(
+            this.card(p?.c7.title, p?.c7.description, p?.c7.href, p?.c7.tags),
+            this.card(p?.c8.title, p?.c8.description, p?.c8.href, p?.c8.tags)
+        )}`;
+    },
+    box(card1, card2) {
+        if (card1 == this.blankCard && card2 == this.blankCard)
+        return '';
 
-class post {
-
-    static blankCard = `<div style="width: 90%; margin: 10px;"><div></div>`;
-
-    static card(title, description, href, tags) {
-
+        return `<section name="post-orientation" class="flex"> 
+            ${card1 ??= ''}${card2 ??= ''}
+        </section>`;
+    },
+    card(title, description, href, tags) {
         let key = false;
 
         for (let i = 0; i < arguments.length; i++) {
             if (arguments[i] != null) key = true;
         };
 
-        return key ? `
-        <article>
+        return key ? `<article>
             <section style="margin: 10px;">
                 <a href=${href ?? '#'}>
                     <div style="font-size: 1.5rem; font-weight: 700;">
@@ -92,10 +112,8 @@ class post {
                 ${this.tag(tags ?? '')}
             </section>
         </article>` : this.blankCard;
-    };
-
-    static contourColor(color) {
-
+    },
+    contourColor(color) {
         color ??= 'transparent';
         const c = color[0] == '#' ? color.replace('#', '') : color;
         const rgb = [
@@ -103,27 +121,25 @@ class post {
             parseInt(`${c[2]}${c[3]}`, 16),
             parseInt(`${c[4]}${c[5]}`, 16)
         ];
-        const brightness = 20;
-        return `rgb(${rgb[0] + brightness }, ${rgb[1] + brightness }, ${rgb[2] + brightness })`
-    };
-
-    static langSelect(plang) {
+        const BRIGHT = 20;
+        return `rgb(${rgb[0] + BRIGHT}, ${rgb[1] + BRIGHT}, ${rgb[2] + BRIGHT})`;
+    },
+    langSelect(plang) {
         let arr = [];
+
         for (let i = 0; i < lang.length; i++) {
             if (plang == lang[i][0]) {
                 arr.push(lang[i][1]);
                 arr.push(lang[i][2]);
             };
         };
-        return `
-        <div style="margin: 0 4px;">
+        
+        return `<div style="margin: 0 4px;">
             <div style="display: inline-block; width: 12px; height: 12px;
             border: solid 1px ${this.contourColor(arr[1])}; border-radius: 100%; background:${arr[1] ??= ''};"></div> ${arr[0] ??= ''}
         </div>`;
-    };
-
-    static tag(tags) {
-
+    },
+    tag(tags) {
         const tagArr = tags.toLowerCase().replace(/\s/g, '').split(',');
         const style = `align-itens: center; justify-content:center; margin: 10px; font-size: 80%; color:var(--tag-description-color);`;
         
@@ -147,42 +163,5 @@ class post {
         </div>`;
 
         return null;
-    };
-    
-    static box(card1, card2) {
-        return `<section name="post-orientation" class="flex"> 
-        ${card1 ??= ''}${card2 ??= ''}
-        </section>`;
-    };
-
-    static error(context, response) {
-        const content = document.getElementById(context);
-        return content == null ? '' : content.innerHTML = response ?? `
-        <article class="flex">
-            <div style="margin: 10px; font-size: 150%;">Data not found!</div>
-        </article>`;
-    };
-    
-    static page(context, p) {
-
-        const content = document.getElementById(context);
-
-        return content == null ? '' : content.innerHTML = `
-        ${this.box(
-            this.card(p?.c1.title, p?.c1.description, p?.c1.href, p?.c1.tags),
-            this.card(p?.c2.title, p?.c2.description, p?.c2.href, p?.c2.tags)
-        )}
-        ${this.box(
-            this.card(p?.c3.title, p?.c3.description, p?.c3.href, p?.c3.tags),
-            this.card(p?.c4.title, p?.c4.description, p?.c4.href, p?.c4.tags)
-        )}
-        ${this.box(
-            this.card(p?.c5.title, p?.c5.description, p?.c5.href, p?.c5.tags),
-            this.card(p?.c6.title, p?.c6.description, p?.c6.href, p?.c6.tags)
-        )}
-        ${this.box(
-            this.card(p?.c7.title, p?.c7.description, p?.c7.href, p?.c7.tags),
-            this.card(p?.c8.title, p?.c8.description, p?.c8.href, p?.c8.tags)
-        )}`;
-    };
+    }
 };
